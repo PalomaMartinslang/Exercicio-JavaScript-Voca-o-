0f1e88345359d8c25fbca978c9e7c99f93030a8c
PalomaMartinslang <palomamartinscontato@gmail.com>
2024-09-26 16:56:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function solicitarDados (){
+    //Solicita o nome do usuário
+    let nome = prompt ("Qual é o seu nome?");
+    //Solicita a idade do usuário
+    let idade = prompt("Qual é a sua idade?");
+    //Solicita a região em que o usuário mora
+    let região = prompt("Qual a região em que mora?");
+    //Solicita o gênero do usuário
+    let gênero = prompt ("Qual é seu gênero?")
+    //Solicita a nacionalidade
+    let nacionalidade = prompt ("Qual é sua nacionalidade?");
+    //Solicita a profissão do usuário
+    let profissão = prompt ("Qual é sua profissão?");
+    alert ()
+
+
+}
